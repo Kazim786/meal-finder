6ae39b39612c661d7f5be82b1fa3d4f9981a0f7e
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-09 16:33:16 -0500

    geetting small error

--- script.js
+++ script.js
@@ -22,8 +22,27 @@ async function searchMeal(e) {
 
     if(term.trim()){
        const meal = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+       console.log(meal)
        console.log(meal.data.meals[0].strMeal)
-    }
+       resultHeading.innerHTML = `<h2>Search Results for '${term}': </h2>`
+
+       if(meal.meals === null){
+           resultHeading.innerHTML = `There are no search results for ${term}. Try again!`
+       }
+       else {
+           mealsEl.innerHTML = meal.meals.map(meal => `
+            <div class= 'meal'>
+                <img src= "${meal.strMealThumb} alt = ${meal.strMeal}" />
+                <div class= "meal-info" data-mealID ="${meal.idMeal}">
+                    <h3>${meal.strMeal}</h3>
+                </div>
+            </div>
+            `)
+            .join('')
+       }
+       //clear search text
+       search.value = ""
+    } 
     else{
         alert('Cant be blank')
     }
